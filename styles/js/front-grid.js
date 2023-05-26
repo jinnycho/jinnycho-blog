f041038e07518f7f1712f63dfadc1a9a29cb6b15
jinnycho <eunjincho503@gmail.com>
2023-05-25 23:40:27 -0400

    cursor following

---
+++ styles/js/front-grid.js
@@ -0,0 +1,24 @@
+let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
+let viewportHeight = window.innerHeight || document.documentElement.clientHeight;
+const gridLength = 15;
+
+window.onload = function() {
+    let gridContainer = document.getElementById('grid-container');
+    for(let i = 0; i < viewportWidth/gridLength; i++){
+        for (let j = 0; j < viewportHeight/gridLength; j++) {
+            const gridElement = document.createElement("div");
+            gridElement.classList.add("grid-item");
+            gridElement.style.backgroundColor = getRandomColor();
+            gridContainer.appendChild(gridElement); 
+        }
+    }
+}
+
+function getRandomColor() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}  
