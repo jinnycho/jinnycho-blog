653ad99acc191a1f46543ec997a5f18d900c5774
jinnycho <eunjincho503@gmail.com>
2023-05-26 13:45:51 -0400

    Add more fonts and update the css

--- styles/js/front.js
+++ styles/js/front.js
@@ -1,8 +1,13 @@
 let viewportWidth = window.innerWidth || document.documentElement.clientWidth;
 let viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-const gridLength = 15;
+const gridLength = 13;
 
 window.onload = function() {
+    // 1. Front text
+    let frontTextContainer = document.getElementById('front-greeting');
+    frontTextContainer.textContent = getFrontTextOptions();
+
+    // 2. Cursor graphics
     let gridContainer = document.getElementById('grid-container');
     for(let i = 0; i < viewportWidth/gridLength; i++){
         for (let j = 0; j < viewportHeight/gridLength; j++) {
@@ -12,6 +17,14 @@ window.onload = function() {
             gridContainer.appendChild(gridElement); 
         }
     }
+}
+
+function getFrontTextOptions() {
+    let frontTextsOptions = [
+        "Hello"
+    ];
+
+    return frontTextsOptions[Math.floor(Math.random() * 3)];
 }
 
 function getRandomColor() {
