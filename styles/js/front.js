f999d06ab0779825eeef305cd4f265b280ea6365
jinnycho <eunjincho503@gmail.com>
2023-05-26 23:30:51 -0400

    Have changing texts

--- styles/js/front.js
+++ styles/js/front.js
@@ -6,6 +6,7 @@ window.onload = function() {
     // 1. Front text
     let frontTextContainer = document.getElementById('front-greeting');
     frontTextContainer.textContent = getFrontTextOptions();
+    frontTextContainer.style.color = getRandomColor();
 
     // 2. Cursor graphics
     let gridContainer = document.getElementById('grid-container');
@@ -20,11 +21,116 @@ window.onload = function() {
 }
 
 function getFrontTextOptions() {
-    let frontTextsOptions = [
-        "Hello"
+    const adjectives = [
+        'Lovely',
+        'Beautiful',
+        'Funny',
+        'Indie',
+        'Sunny',
+        'Windy',
+        'Calm',
+        'Silver',
+        'Golden',
+        'Rainy',
+        'Quiet',
+        'Peaceful',
+        'Cloudy',
+        'Vintage',
+        'Soft',
+        'Gentle',
+        'Sweet',
+        'Serene',
+        'Joyful',
+        'Iconic',
+        'Amazing',
+        'Idyllic',
+        'Elysian',
+        'Rustic',
+        'Delightful',
+        'Charming',
+        'Romantic',
+        'Dramatic',
+        'Lively',
+        'Colorful',
+        'Dazzling',
+        'Sunny',
+        'Warm',
+        'Dreamy',
+        'Favorite',
+        'Grandest',
+        'Cute',
+        'Magical',
+    ];
+    const nouns = [
+        'Place',
+        'Room',
+        'Record',
+        'Space',
+        'Sunset',
+        'Sunrise',
+        'Morning',
+        'Afternoon',
+        'Evening',
+        'Balcony',
+        'Bedroom',
+        'Meadow',
+        'Garden',
+        'Walk',
+        'Stroll',
+        'Piano',
+        'Violin',
+        'Watch',
+        'Winter',
+        'Spring',
+        'Autumn',
+        'Summer',
+        'Moment',
+        'Breakfast',
+        'Coffee',
+        'Tea',
+        'Pastry',
+        'Chocolate',
+        'Dream',
+        'Pasture',
+        'Field',
+        'Farm',
+        'Cottage',
+        'Cabin',
+        'Home',
+        'Apartment',
+        'Patina',
+        'Teatime',
+        'Soirée',
+        'Guitar',
+        'Day',
+        'Month',
+        'Year',
+        'Decade',
+        'Hour',
+        'Bunny',
+        'Kitty',
+        'Puppy',
+        'Forest',
+        'Clearing',
     ];
 
-    return frontTextsOptions[Math.floor(Math.random() * 3)];
+    const adjectiveChosen =  adjectives[Math.floor(Math.random() * 39)];
+    const nounChosen = nouns[Math.floor(Math.random() * 51)];
+    let fullTextToReturn = '';
+    
+    if (isVowel(adjectiveChosen)) {
+        fullTextToReturn += 'An ';
+    } else {
+        fullTextToReturn += 'A ';
+    }
+    fullTextToReturn += adjectiveChosen + '\n' + nounChosen;
+
+    return fullTextToReturn;
+}
+
+function isVowel(myText) {
+    const vowels = ("aeiouAEIOU"); 
+    return vowels.indexOf(myText[0]) !== -1;
 }
 
 function getRandomColor() {
