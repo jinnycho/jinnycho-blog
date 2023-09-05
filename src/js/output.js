d97b5bb5e99978fb470710ae30a7a6b22089e3f1
jinnycho <eunjincho503@gmail.com>
2023-09-05 10:58:32 +0200

    Added a lot more contents

--- src/js/output.js
+++ src/js/output.js
@@ -23518,24 +23518,59 @@
   var dreamHouse001_default = `# I want to build my own house <span style="color:#CDC4C1"> (0.0.1) </span>
 ## 2023/09/04
 
-I find myself constantly thinking about my ideal house, engaging in passionate debates within my own mind about the materials, design, and contents of the house. It might sound a bit crazy, but I can spend hours immersed in these thoughts. I decided to document my ideas to prevent them from fading and to continue refining them. It would also be a good way to communicate when I actually build one with Charles, my soul twin.
+I constantly find myself thinking about my ideal home and living situation, engaging in passionate debates within my own mind about the location, materials, and design of the house I'm going to build. It might sound a bit crazy, but I can spend hours immersed in these thoughts. I decided to document my ideas to prevent them from fading and to continue refining them. It would also be a good way to communicate when I actually build one with Charles - my sweet buddy.
 
 ### Lifestyle
-1. **Urban**
-2. **Nature**
-I've recently purchased a condo in the beautiful neighborhood of Brooklyn, NY. It's right next to Prospect Park. I plan to keep this property not only because of the low mortgage rate but also because I just genuinely like Brooklyn. I've made many fun, ridiculously smart yet genuinely sweet friends, have access to hundreds of excellent grocery stores, numerous job opportunities with high salaries, and great access to the delicious Korean food.
+1. **Urban**: Easy access to smart people, job opportunities, good restaurants (especially Korean), cultural activities, exotic groceries, and fun hangouts with friends.
+2. **Nature**: Easy access to more space, nature, create, farm, grow, meditate, and hike.
+
+I separated them into two criteria because it's very challenging to find both in one place. I recently purchased a nice condo in Brooklyn, NY, which aligns perfectly with my first criterion so I will now focus on the second lifestyle aspect, 'Nature', in this blog post.
 
 ### Basic Requirements
-1. **Money**
-2. **Views**
-3. **Food**
-4. **Safety**
+1. **Capital**: A lump sum of money to purchase land and build a house.
+2. **Income**: Constant source of income to support any additional costs that may arise during/after the process of building a house.
+3. **View**
+4. **Space**: It doesn't have to be gigantic, but space for growing vegetables, crafting, and a couple of animals would be nice.
+5. **Accessible Food & Culture**: It doesn't have to be walkable, but a relatively easy access to groceries and culture, even if it's as small as a farmer's market.
+6. **Nature**: An easy access to the nature - and we prefer the forest over the ocean.
+7. **Clean water**
+8. **Safety**: The place should be safe from human-related crimes and not undergoing any active climate change.
+9. **People**: It'd be nice if the people are friendly and easy to communicate with.
+10. **Hospital**
 
 ### Thinking about a Cozy House
+Once this extensive list is satisfied and we find a place, we can then think about the building itself. The following is a rough list of things that we think it'd be nice to have inside and out. The main theme is "well-designed & sustainable". There are three categories that I'd like to consider:
+- Design(\u{1F485}): Primarily for aesthetics
+- Sustainable(\u{1F333}): Saves excessive use of energy and money
+- Functional(\u2699\uFE0F): Improves the quality of life
 
 #### Exterior
+1. **Balcony**: (\u{1F333}\u2699\uFE0F) With a balcony and additional sunshade, it will help keep the indoors cool in summer and warm in winter because the sun's position changes throughout the seasons. It will also give us easy access to a wider view.
+2. **Raindrop windows**: (\u{1F485}\u2699\uFE0F) Charles loves watching rain drops on the window during rainy days.
+3. **A-shaped roof**: (\u{1F485}\u{1F333}) An A-shaped roof is more weather-resistant compared to a flat roof.
+4. **Solar panels**: (\u{1F333})
+5. **Quality materials that age well**: (\u{1F333}\u{1F485}\u2699\uFE0F) Need further investigation on what it is.
+6. **Places to dry clothes & comforter**: (\u{1F333}\u2699\uFE0F) It is highly energy-efficient, offers natural disinfection, and provides numerous other beneficial effects.
+7. **Sunlight**: (\u{1F333}\u2699\uFE0F)
+8. **Bouldering**: (\u2699\uFE0F) Always though it'd be cool to have one.
+9. **Courtyard**: (\u{1F333}\u2699\uFE0F\u{1F485}) A courtyard brings the outside in, enabling people to enjoy the outdoors more privately and freely. It is also known to help keep the interior cooler in hot weather and warmer in cold weather.
+10. **Food garden**: (\u{1F333}\u2699\uFE0F)
+11. **Ceiling window**: (\u{1F333}\u2699\uFE0F\u{1F485})
+12. **Passive House principles**: (\u{1F333}\u2699\uFE0F)
 
 #### Interior
+1. **Hardwood floor**: (\u{1F333}\u2699\uFE0F\u{1F485}) Expensive but always good to have hardwood floor. I'm wondering if I could make it myself.
+2. **Cross breeze**: (\u{1F333}\u2699\uFE0F)
+3. **Tilt-and-turn windows**: (\u{1F333}\u2699\uFE0F)
+4. **External shades**: (\u{1F333}\u2699\uFE0F) I noticed lots of external shades in Germany. It's a good idea to keep the house cool in summer and warm in winter.
+5. **Fire place**: (\u{1F333}\u2699\uFE0F\u{1F485})
+6. **Thermal heating**: (\u{1F333}\u2699\uFE0F)
+7. **Air purifier**: (\u{1F333}\u2699\uFE0F)
+8. **HRV**: (\u{1F333}\u2699\uFE0F)
+9. **Crafting space**: (\u2699\uFE0F) We always wish to have a dedicated space for crafting, such as wood/metalworking, watchmaking, and drafting. This space could also have an office space if needed for remote work.
+10. **Ceiling fans**: (\u{1F333}\u2699\uFE0F) Heard that it really cools down the space without having to use A/C.
+11. **Windows @ kitchen, bathrooms**: (\u{1F333}\u2699\uFE0F\u{1F485}) Ventilation is important in these areas and natural sunlight is always a plus.
+12. **Kitchen island**:
 `;
 
   // node_modules/marked/lib/marked.esm.js
--- src/writings/ideas/architecture/dreamHouse001.md
+++ src/writings/ideas/architecture/dreamHouse001.md
@@ -1,21 +1,56 @@
 # I want to build my own house <span style="color:#CDC4C1"> (0.0.1) </span>
 ## 2023/09/04
 
-I find myself constantly thinking about my ideal house, engaging in passionate debates within my own mind about the materials, design, and contents of the house. It might sound a bit crazy, but I can spend hours immersed in these thoughts. I decided to document my ideas to prevent them from fading and to continue refining them. It would also be a good way to communicate when I actually build one with Charles, my soul twin.
+I constantly find myself thinking about my ideal home and living situation, engaging in passionate debates within my own mind about the location, materials, and design of the house I'm going to build. It might sound a bit crazy, but I can spend hours immersed in these thoughts. I decided to document my ideas to prevent them from fading and to continue refining them. It would also be a good way to communicate when I actually build one with Charles - my sweet buddy.
 
 ### Lifestyle
-1. **Urban**
-2. **Nature**
-I've recently purchased a condo in the beautiful neighborhood of Brooklyn, NY. It's right next to Prospect Park. I plan to keep this property not only because of the low mortgage rate but also because I just genuinely like Brooklyn. I've made many fun, ridiculously smart yet genuinely sweet friends, have access to hundreds of excellent grocery stores, numerous job opportunities with high salaries, and great access to the delicious Korean food.
+1. **Urban**: Easy access to smart people, job opportunities, good restaurants (especially Korean), cultural activities, exotic groceries, and fun hangouts with friends.
+2. **Nature**: Easy access to more space, nature, create, farm, grow, meditate, and hike.
+
+I separated them into two criteria because it's very challenging to find both in one place. I recently purchased a nice condo in Brooklyn, NY, which aligns perfectly with my first criterion so I will now focus on the second lifestyle aspect, 'Nature', in this blog post.
 
 ### Basic Requirements
-1. **Money**
-2. **Views**
-3. **Food**
-4. **Safety**
+1. **Capital**: A lump sum of money to purchase land and build a house.
+2. **Income**: Constant source of income to support any additional costs that may arise during/after the process of building a house.
+3. **View**
+4. **Space**: It doesn't have to be gigantic, but space for growing vegetables, crafting, and a couple of animals would be nice.
+5. **Accessible Food & Culture**: It doesn't have to be walkable, but a relatively easy access to groceries and culture, even if it's as small as a farmer's market.
+6. **Nature**: An easy access to the nature - and we prefer the forest over the ocean.
+7. **Clean water**
+8. **Safety**: The place should be safe from human-related crimes and not undergoing any active climate change.
+9. **People**: It'd be nice if the people are friendly and easy to communicate with.
+10. **Hospital**
 
 ### Thinking about a Cozy House
+Once this extensive list is satisfied and we find a place, we can then think about the building itself. The following is a rough list of things that we think it'd be nice to have inside and out. The main theme is "well-designed & sustainable". There are three categories that I'd like to consider:
+- Design(💅): Primarily for aesthetics
+- Sustainable(🌳): Saves excessive use of energy and money
+- Functional(⚙️): Improves the quality of life
 
 #### Exterior
+1. **Balcony**: (🌳⚙️) With a balcony and additional sunshade, it will help keep the indoors cool in summer and warm in winter because the sun's position changes throughout the seasons. It will also give us easy access to a wider view.
+2. **Raindrop windows**: (💅⚙️) Charles loves watching rain drops on the window during rainy days.
+3. **A-shaped roof**: (💅🌳) An A-shaped roof is more weather-resistant compared to a flat roof.
+4. **Solar panels**: (🌳)
+5. **Quality materials that age well**: (🌳💅⚙️) Need further investigation on what it is.
+6. **Places to dry clothes & comforter**: (🌳⚙️) It is highly energy-efficient, offers natural disinfection, and provides numerous other beneficial effects.
+7. **Sunlight**: (🌳⚙️)
+8. **Bouldering**: (⚙️) Always though it'd be cool to have one.
+9. **Courtyard**: (🌳⚙️💅) A courtyard brings the outside in, enabling people to enjoy the outdoors more privately and freely. It is also known to help keep the interior cooler in hot weather and warmer in cold weather.
+10. **Food garden**: (🌳⚙️)
+11. **Ceiling window**: (🌳⚙️💅)
+12. **Passive House principles**: (🌳⚙️)
 
 #### Interior
+1. **Hardwood floor**: (🌳⚙️💅) Expensive but always good to have hardwood floor. I'm wondering if I could make it myself.
+2. **Cross breeze**: (🌳⚙️)
+3. **Tilt-and-turn windows**: (🌳⚙️)
+4. **External shades**: (🌳⚙️) I noticed lots of external shades in Germany. It's a good idea to keep the house cool in summer and warm in winter.
+5. **Fire place**: (🌳⚙️💅)
+6. **Thermal heating**: (🌳⚙️)
+7. **Air purifier**: (🌳⚙️)
+8. **HRV**: (🌳⚙️)
+9. **Crafting space**: (⚙️) We always wish to have a dedicated space for crafting, such as wood/metalworking, watchmaking, and drafting. This space could also have an office space if needed for remote work.
+10. **Ceiling fans**: (🌳⚙️) Heard that it really cools down the space without having to use A/C.
+11. **Windows @ kitchen, bathrooms**: (🌳⚙️💅) Ventilation is important in these areas and natural sunlight is always a plus.
+12. **Kitchen island**:
