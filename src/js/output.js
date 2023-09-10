54e741d968c2f567bee356049838eba965293854
jinnycho <eunjincho503@gmail.com>
2023-09-10 19:25:57 -0400

    Finish the book

--- src/js/output.js
+++ src/js/output.js
@@ -23576,18 +23576,20 @@ Once this extensive list is satisfied and we find a place, we can then think abo
 
   // src/writings/books/psychology/courageToBeDisliked.md
   var courageToBeDisliked_default = `# Courage To be Disliked
-## 2023/09/08
+## 2023/09/10
 
-If I choose the most impactful book this year, <Courage to be Disliked> would be the one. This is the book based off of Adlerian psychology. I'd like to keep a note of quotes that resonate a lot to me with some additional thoughts.
+If I were to choose the most impactful book of this year, 'Courage to Be Disliked' would be the one. This book is based on Adlerian psychology, and I'd like to keep a record of quotes that resonate with me, along with some additional thoughts.
 
-### It's me that makes the world
-> This is not because the world is complicated. It's because you are making the world complicated.
-
+### 1. It's me that makes the world
 > Well water stays at pretty much the same temperature all year around - but when you drink the water in the summer it seems cool, and when you drink the same water in the winter it seems warm.
 
-> It's as if you see the world through dark glasses, so naturally everything seems dark.
+> This is not because the world is complicated. It's because you are making the world complicated. ... It's as if you see the world through dark glasses, so naturally everything seems dark.
 
-### Sick of "trauma" 
+This idea aligns with Buddhism. The wise monk Huineng once said, 'Neither the wind nor the flag is moving.' When asked, 'Then what is it that is moving?' he replied, 'Your mind is moving.' 
+
+At the end of the day, the world is perceived through my eyes, and it is inherently subjective. This idea is refreshingly enlightening and invigorating because it empowers me to take the initiative to see the world more clearly and postively.
+
+### 2. Sick of "trauma" 
 > Who I am now (the effect) is determined by occurrences in the past (the causes) ... If we focus only on past causes and try to explain things soley through cause and effect, we end up with "determinism". Because what this says is that our present and future have already been decided by past occurrences, and are unalterable.
 
 > In Adlerian psychology, we do not think about past "causes" but rather about present "goals". For example, if there's a guy, who has shut himself in his room for years, in Adlerian psychology, he had a goal of not going out beforehand and he's been manufacturing a state of anxiety and fear as a means to achieve the goal.
@@ -23596,14 +23598,16 @@ If I choose the most impactful book this year, <Courage to be Disliked> would be
 
 > In Adlerian psychology, trauma is definitively denied. ... We are not determined by our experiences, but the meaning we give them is self-determining.
 
-### People change
+I noticed that the term 'trauma' is often overused, with many people attempting to explain issues by referring to past traumas rather than addressing the real problems. Personally, I haven't found this approach that useful; in fact, it has been more tormenting than helpful. I've discovered that not fixating on 'trauma' is surprisingly beneficial for me in overcoming pain. Again this was also very much in line with Buddhism.
+
+### 3. People Can Change
 > You could consider that issue is not personality but rather the view of the world. It seems that the world "personality" is nuanced and suggests being unchangeable. But if we are talking about a view of the world, that should be possible to alter.
 
 > People can change at anytime, regardless of the environments they are in. You are unable to change only because you are making a decision not to. 
 
 > Not change - you could say it's like driving your old, familiar car. It might rattle a bit, but one can take that into account and maneuver easily. When we try to change our lifestyle, we put our great courage to the test.
 
-### Am I just making excuses?
+### 4. Am I just making excuses?
 > He wants to live inside the realm of possiblities, where he can say that he could do it if he only had the time, or that he could write if he just had the proper environment, and that he really does have the talent for it.
 
 > I won't get any work done if I'm shunned by my direct superior. That's Adler's life-lie again. I can't do my work because I was shunned by my boss. It's the boss's fault that my work isn't going well. ... I don't want to work, so I'll create an awful boss, or I don't want to acknowledge my incapable self, so I'll create an awful boss.
@@ -23612,7 +23616,11 @@ If I choose the most impactful book this year, <Courage to be Disliked> would be
 
 > For me, it was more convenient not to repair my relationship with my father. I could use having a father like that as an excuse for why my own life wasn't going well.
 
-### All Problems are Interpersonal Problems
+This part also resonates with me. During a recent trip to Berlin with Charles, I found myself feeling less attractive compared to other girls on the street. However, I later realized that my insecurities were likely stemming from anxiety and nervousness in the new environment, leading me to degrade my own appearance as an excuse. 
+
+Equally interestingly, this unhealthy mindset revived my hierarchical and competitive tendencies that I'm accustomed to, causing some troubles during the trip. I appreciate the opportunity to reacquaint myself with Adlerian psychology and recognize that such self-degrading mindset is not in line with the healthy person I aspire to be.
+
+### 5. All Problems are Interpersonal Problems
 > It's basically impossible to not get hurt in your relations with other people.
 
 > Let's learn a way to settle things without using the emotion of anger. Because after all, anger is a tool. A means for achieving a goal. 
@@ -23621,16 +23629,16 @@ If I choose the most impactful book this year, <Courage to be Disliked> would be
 
 > You had the goal of taking a dislike to Mr. A beforehand and then started looking for the flaws to satisfy the goal. So that you could avoid an interpersonal relationship with Mr. A. 
 
-### Inferiority & Superiority Complex
+### 6. Inferiority & Superiority Complex
 > You notice only your shortcomings because you've resolved to not start liking yourself. ... You've avoided interpersonal relationships by dislking yourself.
-
-I think similar thing happened when I was travelling in Berlin. It ended up being such a different place, I started to try to find things that I dislike about myself - just to avoid the situation?
 
 > It's crucial to not mix up "feeling of inferiority" and "inferiority complex", and to think about them as clearly separate. There is nothing particularly wrong with the feeling of inferiority itself. The feeling of inferiority can be a trigger for striving and growth. The inferiority complex, on the other hand, refers to a condition of having begun to use one's feeling of inferiority as a kind of excuse and people who aren't equipped with courage end up stepping into an inferiority complex.
 
 > Those who make themselves look bigger on borrowed power are essentially living according to other people's value systems - they're living other people's lives... Those who go so far as to boast about things out loud actually have no confidence in themselves. "The one who boasts does so only out of a feeling of inferiority."
 
-### Competition Bullshit
+Inferiority/Superiority complex is closely related to the competitive mindset, where people perceive the world strictly hierarchical. All of them are inherently toxic and will not make anyone happy, whether you're the winner or the loser in this context. This is simply BAD, and I should be careful every time. Even a single thought of hierarchy can jeopardize a good relationship, causing numerous troubles from within.
+
+### 7. Competition Bullshit
 > It's enough to just keep moving in a forward direction, without competing with anyone. And, of course, there is no need to compare oneself with others. ... A healthy feeling of inferiority is not something that comes from comparing oneself to others; it comes from one's comparison with one's ideal self.
 
 > Everyone is different. Don't mix up that difference with good and bad, and superior or inferior. Whatever differences we may have, we are all equal.
@@ -23639,15 +23647,13 @@ I think similar thing happened when I was travelling in Berlin. It ended up bein
 
 > The reason so many people don't really feel happy while they're building up their success in the eyes of society is that they are living in competition. Because to them, the world is a perilous place that is overflowing with enemies.
 
-This is what I've been unconsciously feeling recently when I started the project with another teammate. Don't try to compete for god sake!
-
 > If someone were to abuse me to my face, I would think about the person's hidden goal. Even if you're not directly abusive, when you feel genuinely angry due to another person's words or behavior, please consider that the person is challenging you to a power struggle. And one must not allow oneself to be taken in.
 
 > One more thing about power struggles. In every instance, no matter how much you might think you are right, try not to criticize the other party on that basis. The moment one is convinced that "I am right", one has already stepped into a power struggle. .. Then many people will rush into a power struggle and try to make other submit to them. ... Because of one's mindset of not wanting to lose, one is unable to admit one's mistake, the result being that one ends up choosing the wrong path. ... it clouds your judgment.
 
-This happened a few times during the travel too. I really should be careful with the competitive mindset.
+This train of thought has occurred a few times while I'm working as well. I really should be cautious with the competitive mindset. It not only fails to improve my productivity but also disrupts my inner peace and happiness.
 
-### Objectives in Life
+### 8. Objectives in Life
 > to be self-reliant and to live in harmony with society ... consciousness that I have the ability and the consciousness that people are my comarades
 
 > If you change, those around you will change too. Adlerian psychology is a psychology for changing onself - not others.
@@ -23656,10 +23662,7 @@ This happened a few times during the travel too. I really should be careful with
 
 > It is when one is able to feel "I am beneficial to the community" that one can have a true sense of one's worth.
 
-### Love
-> Whenever I'm with this person, I can behave very freely, one can really feel love. One can be in a calm and quite natural state, without having feelings of inferiority or being beset with the need to flaunt one's superiority. That is what real love is like.
-
-### Recognition Bullshit
+### 9. Recognition Bullshit
 > There is no need to be recognized by others. Actually, one must not seek recognition. This point cannot be overstated.
 
 > Adler was very critical of education by reward and punishment. If no one is going to praise me, I won't take appropriate actions and if no one is going to punish me, I'll engage in inappropriate actions, too.
@@ -23676,12 +23679,10 @@ This happened a few times during the travel too. I really should be careful with
 
 > "Judgment" is a word that comes out of vertical relationships. If one is building horizontal relationships, there will be words of more straightforward gratitude and repsect and joy. 
 
-### My task vs. Your task
+### 10. My task vs. Your task
 > One does not intrude on other people's tasks. In general, all interpersonal relationship troubles are caused by intruding on other people's tasks.
 
 > Calmly delieate up to what point one's own tasks go, and from what point they become another person's tasks. And do not intervene in other people's tasks, or allow even a single person to intervene one's own tasks.
-
-This gets me thinking a lot about my current work with my coworker. Am I being too controlling and intrusive? I think I was being a little insecure because of the frontend stuff. You're not competing with this person.
 
 > When reading a book, if one brings one's face too close to it, one cannot see anything. In the same way, forming good relationships requires a certain degree of distance.
 
@@ -23689,7 +23690,9 @@ This gets me thinking a lot about my current work with my coworker. Am I being t
 
 > You are not the center of the world - other people are not living to satisfy your expectations.
 
-### Community Feeling
+This makes me reflect on my current work with my coworker. Am I being too controlling and intrusive? I realize that my actions may have stemmed from a sense of insecurity regarding the frontend work that I'm not very familiar with. It's important to remember that we're not in competition with each other.
+
+### 11. Community Feeling
 > You will see that all the hardship you went through in school was a storm in a teacup. The moment you leave the teacup, that raging storm will be gone, and a gentle breeze will greet you in its place.
 
 > If unreasonable demands are being thrust on you, it is fine to object to them directly. Do not cling to the small community right in front of you.
@@ -23698,7 +23701,9 @@ This gets me thinking a lot about my current work with my coworker. Am I being t
 
 > When one feels "I am of use to someone.", contriubtion to others, rather than being about getting rid of the "I" and being service to someone, is actually something one does in order to be truly aware of the worth of the "I".
 
-### Horizontal relationships
+### 12. Horizontal relationships
+> Whenever I'm with this person, I can behave very freely, one can really feel love. One can be in a calm and quite natural state, without having feelings of inferiority or being beset with the need to flaunt one's superiority. That is what real love is like.
+
 > If you are building even one vertical relationship with someone, before you even notice what is happening, you will be treating all your interpersonal relations as vertical.
 
 > What is important is to be equal in consciousness, and to assert that which needs to be asserted.
@@ -23707,14 +23712,16 @@ This gets me thinking a lot about my current work with my coworker. Am I being t
 
 > Unconditional confidence iis a means for making your interpersonal relationship with a person better and for building a horizontal relationship. If you do not have the desire to make your relationship with that person better, go ahead and sever it.
 
-> If it is a shallow relationship, when it falls apart the pain will be slight. And the joy that relationship brings each day will also be slight. It is precisely because one can gain the courage to enter into deepr relationships by having confidence in others that the joy of one's interpersonal relations can grow and one's joy in life can grow too.
+> If it is a shallow relationship, when it falls apart the pain will be slight. And the joy that relationship brings each day will also be slight. It is precisely because one can gain the courage to enter into deeper relationships by having confidence in others that the joy of one's interpersonal relations can grow and one's joy in life can grow too.
 
 > Placing confidence in others is connected to seeing others as comrades.
 
-### Self Acceptance
+I appreciate the opportunity to build a deeper relationship with Charles. This relationship has allowed me to reflect more thoroughly on my life. Wanting to spend quality time with him has helped me recognize some unhealthy tendencies I have in certain situations, which have caused certain troubles. For example, I've noticed that I sometimes become competitive with Charles, thinking 'I'm right, he's wrong, and I need to win.' This mindset has caused several interpersonal issues - and, after all, even if I won, I felt bad.
+
+### 13. Self Acceptance
 > With self-acceptance, if one cannot do something, one is simply accepting "one's incapable self" as is and moving forward so that one can do whatever one can. ... One simply has to focus on what one can chamge, rather than on what one cannot. ... affirmative resignation.
 
-### Freedom & Harmony
+### 14. Freedom & Harmony
 > Do not be dependent on vertical relationships or be afraid of being disliked, and just make your way forward freely.
 
 > A way of living that acknowledges only company work is one that is lacking in harmony of life.
