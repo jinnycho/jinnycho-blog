a60c83a312cbb785e985706c7a91303e09ebba8d
Jinny Cho <45345735+jinnycho@users.noreply.github.com>
2023-10-15 23:38:22 -0400

    Add a doc about blog (#44)
    * Add a doc about blog
    
    * Add more contents
    
    * Add final touch

--- src/js/Config.jsx
+++ src/js/Config.jsx
@@ -1,41 +1,48 @@
 import dreamingHouse001Text from '../writings/ideas/architecture/dreamHouse001.md';
 import courageToBeDislikedText from '../writings/books/psychology/courageToBeDisliked.md';
-import myFirstTableText from '../writings/create/woodworking/myFirstTable.md';
+import myFirstTableText from '../writings/hobbies/woodworking/myFirstTable.md';
+import myBlogHistoryText from '../writings/tech/create/myBlogHistory.md';
 
 const categoryIDToValue = {
     'ideas': 'Ideas',
     'books': 'Books',
-    'create': 'Create',
+    'hobbies': 'Hobbies',
+    'tech': 'Tech',
 };
 
 const categoryIDTosubCategoriesIDs = {
     'ideas': ['architecture'],
     'books': ['psychology'],
-    'create': ['woodworking'],
+    'hobbies': ['woodworking'],
+    'tech': ['create'],
 };
 
 const subCategoryIDToValue = {
     'architecture': 'Architecture',
     'psychology': 'Psychology',
     'woodworking': 'Woodworking',
+    'create': 'Create',
 }
 
 const subCategoryIDToTitleIDs = {
     'architecture': ['houseIdeas001'],
     'psychology': ['courageToBeDisliked'],
     'woodworking': ['myFirstTable'],
+    'create': ['myBlogHistory'],
 }
 
 const contentsTitleIDToValue = {
     'houseIdeas001': 'I Want To Build My Own House (0.0.1)',
     'courageToBeDisliked': 'Courage To Be Disliked',
-    'myFirstTable': 'The First Table I Made'
+    'myFirstTable': 'The First Table I Made',
+    'myBlogHistory': 'The Evolution of My Blogging Journey',
 }
 
 const contentsTitleIDToContentsContentValue = {
     'houseIdeas001': dreamingHouse001Text,
     'courageToBeDisliked': courageToBeDislikedText,
     'myFirstTable': myFirstTableText,
+    'myBlogHistory': myBlogHistoryText,
 }
 
 export {
