c8114e82a084f83553f210a92542f321ef2b23c2
jinnycho <eunjincho503@gmail.com>
2023-11-24 22:53:39 -0500

    Add more contents

--- src/js/output.js
+++ src/js/output.js
@@ -27654,7 +27654,26 @@ Once this extensive list is satisfied and I find a place, I can then think about
 ![Alt Text alt ><](https://media.giphy.com/media/ianAz6rcKfjoY/giphy.gif)`;
 
   // src/writings/ideas/travel/joshuaTree.md
-  var joshuaTree_default = "# Why I Refuse to Call It a Joshua Tree\n\n## 2023/11/25\n\nWe parked our campervan near the Cholla Cactus Garden in Joshua Tree National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt like I time-travelled back to hundreds of thousands of years ago. Similarly at Zion National Park, everywhere I go was the awe-inspiring sight of mountains, sky and valleys. It felt as if I were taking a look at the landscape through the eyes of the Native Americans back in the days, who must have had similarly deep appreciation for the view before me. As much as I love these places, I strongly think we should call them differently. ";
+  var joshuaTree_default = `# Why I Refuse to Call It a Joshua Tree
+
+## 2023/11/25
+
+We parked our campervan near the Cholla Cactus Garden in _Joshua Tree_ National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt as if I had traveled back hundreds of thousands of years. Similarly at _Zion_ National Park, everywhere I go was the awe-inspiring sight of mountains, stars, and valleys. It felt as if I were looking at the landscape through the eyes of the Native Americans from bygone days who must have had similarly deep appreciation for the view. The more I love these places, the more strongly I think we should call them differently. 
+
+### It's Not "Joshua Tree"
+
+![JoshuaTree 1 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/joshuaTree1.png?raw=true)
+
+For thousands of years, many different native American groups embraced this giant Yucca tree as a spiritual reference and valued resource. Southern Paiute called it "**sovarampi**". Cahuilla Indians referred to it as "**humwichawa**" and the Western Shoshone knew it simply as "**umpu**". Shouldn't we consider using one of these Native American names as a way to show respect for their cultural connection to the land? 
+
+At the very least, using a completely unrelated biblical name **Joshua** seems out of context for this desert in California. Apparently, the name "Joshua tree" was given by a group of Mormon settlers who crossed the Mojave Desert in the mid-19th century. Holding onto a Christian name given just 200 years ago not only disrespects the Native Americans who lived in the area for thousands of years but also hinders educational opportunities for children. Learning more about Native American culture can prevent the repetition of similar tragedy in the future. Name matters.
+
+If choosing any one of the Native American names is hard, the tree honestly looks more like a dragon tail or caterpillar. And I honestly called it "Dragon Tail Tree" the whole time I was there.
+
+### Same goes Mt.Ryan or Zion
+
+![JoshuaTree 2 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/joshuaTree2.png?raw=true)
+`;
 
   // src/writings/books/psychology/courageToBeDisliked.md
   var courageToBeDisliked_default = `# Courage To be Disliked
