ffa67049a6d8d0c4d30390adb785c5446c8b405d
jinnycho <eunjincho503@gmail.com>
2023-11-24 23:06:18 -0500

    Add more contents

--- src/js/output.js
+++ src/js/output.js
@@ -27658,7 +27658,7 @@ Once this extensive list is satisfied and I find a place, I can then think about
 
 ## 2023/11/25
 
-We parked our campervan near the Cholla Cactus Garden in _Joshua Tree_ National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt as if I had traveled back hundreds of thousands of years. Similarly at _Zion_ National Park, everywhere I go was the awe-inspiring sight of mountains, stars, and valleys. It felt as if I were looking at the landscape through the eyes of the Native Americans from bygone days who must have had similarly deep appreciation for the view. The more I love these places, the more strongly I think we should call them differently. 
+We parked our campervan near the Cholla Cactus Garden in _Joshua Tree_ National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt as if I had traveled back hundreds of thousands of years. Similarly at _Zion_ National Park, everywhere I go was the awe-inspiring sight of mountains, stars, and valleys. It felt as if I were looking at the landscape through the eyes of the Native Americans from bygone days who must have had similarly deep appreciation for the view. The more I love these places, the more I believe we should give them different names. 
 
 ### It's Not "Joshua Tree"
 
@@ -27666,13 +27666,15 @@ We parked our campervan near the Cholla Cactus Garden in _Joshua Tree_ National 
 
 For thousands of years, many different native American groups embraced this giant Yucca tree as a spiritual reference and valued resource. Southern Paiute called it "**sovarampi**". Cahuilla Indians referred to it as "**humwichawa**" and the Western Shoshone knew it simply as "**umpu**". Shouldn't we consider using one of these Native American names as a way to show respect for their cultural connection to the land? 
 
-At the very least, using a completely unrelated biblical name **Joshua** seems out of context for this desert in California. Apparently, the name "Joshua tree" was given by a group of Mormon settlers who crossed the Mojave Desert in the mid-19th century. Holding onto a Christian name given just 200 years ago not only disrespects the Native Americans who lived in the area for thousands of years but also hinders educational opportunities for children. Learning more about Native American culture can prevent the repetition of similar tragedy in the future. Name matters.
+At the very least, using a completely unrelated biblical name **Joshua** seems out of context for this desert in California. Apparently, the name "Joshua tree" was given by a group of Mormon settlers who crossed the Mojave Desert in the mid-19th century. Holding onto a Christian name that was given barely 200 years ago not only disrespects the Native Americans who lived in the area for thousands of years but also hinders educational opportunities for future generations. Learning more about Native American culture can prevent the repetition of similar tragedy in the future. Name matters.
 
-If choosing any one of the Native American names is hard, the tree honestly looks more like a dragon tail or caterpillar. And I honestly called it "Dragon Tail Tree" the whole time I was there.
+If choosing any one of the Native American names is hard, the tree honestly looks more like a dragon tail or caterpillar. And I called it "Dragon Tail Tree" the whole time I was there.
 
-### Same goes Mt.Ryan or Zion
+### Same goes Zion and more
 
-![JoshuaTree 2 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/joshuaTree2.png?raw=true)
+![JoshuaTree 2 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/joshuaTree3.png?raw=true)
+
+Similarly Zion National Park was called "Mukuntuweap" for thousands years even until 1917. Honestly this transition from Mukuntuweap to Zion is ridiculous. Every mountain peak was also so religious like Angels Landing, Temple of Sinawava, Abraham, Isaac... I thought it was a joke. If changing name is this easy, we should change it back.
 `;
 
   // src/writings/books/psychology/courageToBeDisliked.md
