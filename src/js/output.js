1986b4555f62078260ecb9d0bd73cf1433d63709
jinnycho <eunjincho503@gmail.com>
2023-11-24 22:12:56 -0500

    Add more contents

--- src/js/output.js
+++ src/js/output.js
@@ -27654,7 +27654,7 @@ Once this extensive list is satisfied and I find a place, I can then think about
 ![Alt Text alt ><](https://media.giphy.com/media/ianAz6rcKfjoY/giphy.gif)`;
 
   // src/writings/ideas/travel/joshuaTree.md
-  var joshuaTree_default = "# Why I Refuse to Call It a Joshua Tree\n\n## 2023/11/25\n\nWe parked our campervan near the Cholla Cactus Garden in Joshua Tree National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt like I time-travelled back to hundreds of thousands of years ago. I felt similarly impressed with Zion National Park, where I can imagine the native Americans running and loving the place around. As much as I love this place, I think we should call it differently. \n\n![JoshuaTree 1 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/josuhaTree1.png?raw=true)\n";
+  var joshuaTree_default = "# Why I Refuse to Call It a Joshua Tree\n\n## 2023/11/25\n\nWe parked our campervan near the Cholla Cactus Garden in Joshua Tree National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt like I time-travelled back to hundreds of thousands of years ago. Similarly at Zion National Park, everywhere I go was the awe-inspiring sight of mountains, sky and valleys. It felt as if I were taking a look at the landscape through the eyes of the Native Americans back in the days, who must have had similarly deep appreciation for the view before me. As much as I love these places, I strongly think we should call them differently. ";
 
   // src/writings/books/psychology/courageToBeDisliked.md
   var courageToBeDisliked_default = `# Courage To be Disliked
--- src/writings/ideas/travel/joshuaTree.md
+++ src/writings/ideas/travel/joshuaTree.md
@@ -2,6 +2,4 @@
 
 ## 2023/11/25
 
-We parked our campervan near the Cholla Cactus Garden in Joshua Tree National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt like I time-travelled back to hundreds of thousands of years ago. I felt similarly impressed with Zion National Park, where I can imagine the native Americans running and loving the place around. As much as I love this place, I think we should call it differently. 
-
-![JoshuaTree 1 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/josuhaTree1.png?raw=true)
+We parked our campervan near the Cholla Cactus Garden in Joshua Tree National Park, gazing upon the breathtaking view of the pristine plain, surrounded by majestic mountains and rocks. It felt like I time-travelled back to hundreds of thousands of years ago. Similarly at Zion National Park, everywhere I go was the awe-inspiring sight of mountains, sky and valleys. It felt as if I were taking a look at the landscape through the eyes of the Native Americans back in the days, who must have had similarly deep appreciation for the view before me. As much as I love these places, I strongly think we should call them differently. 
