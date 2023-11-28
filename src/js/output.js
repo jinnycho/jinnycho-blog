5bcdfb83b9a98b2357739e0f6037f8f04dadef91
jinnycho <eunjincho503@gmail.com>
2023-11-28 18:08:19 -0500

    Update

--- src/js/output.js
+++ src/js/output.js
@@ -28058,41 +28058,48 @@ Did you know that platypuses are missing this major organ, and they don't have n
 
 ### 1. People didn't believe it was real
 
-English zoologist George Shaw first wrote about the platypuses in 1799 with some notes & a specimen. Nobody believed it was real and tried to find the stitches around the beak.
+English captain John Hunter first wrote about the platypuses in 1797 with some sketches & a specimen. Nobody believed it was real.
 
 ![Platypus 1 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/platypus1.png?raw=true)
 
-### 2. Their beak is goddamn powerful
+### 2. Its beak is goddamn powerful
 
 What appears to be a beak is actually part of its skull. And apparently it's quite soft in the middle.
 
-This beak is not only aesthetically cute but also very powerful. It allows platypuses to detect underwater prey using electrical impulses (a.k.a electroreception) to locate objects in the deepest and darkest of waters. This is why platypuses close their eyes a lot because they don't need them.
+This beak is not just aesthetically cute; it's also incredibly powerful. It allows a platypus to detect underwater prey using electrical impulses (a.k.a electroreception) to locate objects in the deepest and darkest of waters. This is why platypuses often close their eyes when moving because they use their beaks.
 
-But unfortuantely they don't have teeth. So they must scoop up bits of gravel with its food to help \u201Cchew\u201D its meal. XD
+But unfortuantely it doesn't have teeth. So it must scoop up bits of gravel with its food to help \u201Cchew\u201D its meal. XD
 
 ![Platypus 2 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/platypus2.png?raw=true)
 
-### 3. But they don't have stomach
+### 3. But it doesn't have stomach
 It's unclear why they lack a stomach, but indeed, platypuses are one of the two mammals that don't have a stomach. Perhaps their diet makes a stomach unnecessary.
 
 ### 4. And they don't have nipples
 As egg-laying mammals, platypuses also have a unique method for nursing their babies. They don't have nipples. They "sweat" milk through specialized glands in the skin. 
 
-Because of this strange feeding system, which can be challenging for babies, platypuses have developed milk with highly antibacterial properties and extremely high iron content. Scientists are trying to battle the global superbug threat with platypuses' milk. Go platypuses!
+Because of this strange feeding system, which can be challenging for babies, platypuses have developed milk with highly antibacterial properties and extremely high iron content. Scientists are trying to solve the global superbug threat with platypuses' milk. Go platypuses!
 
-### 5. But they're venomous lol
-Male platypuses have spurs that are connected to a venom gland. It is lethal enough to kill a medium-sized dog but not humans (though it's still excruciatingly painful & no antivenom so you just gotta suffer). The venom is more plentiful during breeding season, leading scientists to believe that it is used to defend mates and resources from rival males.
+### 5. And they're venomous too
+Male platypuses have spurs that are connected to a venom gland. It is lethal enough to kill a medium-sized dog but not humans (though it's still excruciatingly painful & there is no antivenom so be careful). The venom is more plentiful during the breeding season, leading scientists to believe that it is used to defend mates and resources from rival males.
 
 ![platypus venom alt ><](https://upload.wikimedia.org/wikipedia/commons/3/3d/Platypus_spur.JPG)
 
 ### 6. They can drown pretty easily
-Even though they are aquatic animals, they can drown pretty easily. Normally, a platypus stays underwater for between one and 2 minutes. So "Opera house\u201D nets that people set to catch crayfish and yabbies can be death traps for platypuses. BE CAREFUL!
+Even though it sounds like they're a badass animal, they can drown pretty easily. Normally, a platypus stays underwater for between 1 and 2 minutes. So "Opera house\u201D nets that people set to catch crayfish and yabbies can be death traps for platypuses. BE CAREFUL!
 ![Platypus 3 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/platypus3.png?raw=true)
 
-### 7. Last but not least, they are biofluorescent
+### 7. They are biofluorescent
 Under the UV light, they are all glowing. Scientists think it's probably because they're nocturnals so being biofluorescent help them see & communicate each other.
 
 ![platypus biofluorescent alt ><](https://cdn.mos.cms.futurecdn.net/J29hZbBGEePokbrTdRYCEi.jpg)
+
+### 8. Last but not least, they maintain a stable body heat
+Its fur is waterproof and traps an insulating layer of air to keep its body temperature stable, even in cold water.
+
+![platypus scratching alt ><](https://media.giphy.com/media/kyuhMhJnpGnz9C7TT4/giphy-downsized.gif)
+
+Isn't it incredible that there is a semi-aquatic, egg-laying, venomous, electrolocating, milk-sweating, biofluorescent, stomachless, teethless, temperature-controlled animal that's also super adorable?
 
 References
 - [washingtonpost](washingtonpost.com/news/speaking-of-science/wp/2015/04/01/the-platypus-is-so-weird-that-scientists-thought-the-first-specimen-was-a-hoax)
