978d20efc8129429c349010e318ba7d700f3141d
jinnycho <eunjincho503@gmail.com>
2024-04-30 23:35:19 -0400

    Update

--- src/js/output.js
+++ src/js/output.js
@@ -28052,14 +28052,14 @@ It was a delightful 4 weeks. Waking up early and walking there for 40 minutes in
   var newjeans_default = `# New Jeans
 ## 2024/04/30
 
+![newjeans1 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/newjeans1.gif?raw=true)
+
 <audio controls>
   <source src="https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/background.mp4?raw=true" type="audio/mp3">
 </audio>
-\u{1F430} I'll have to share my favorite first.
+\u{1F430} my favorite hit track
 
-
-I've got to tell y'all about one of my favorite Kpop singers I'm obsessed right now. Plus, I'm gonna talk about this lawsuit going on with Ador (New Jeans) and Hybe that will really impact New Jeans' future.
-
+I've got to tell you all about this Kpop group I'm obsessed with right now. Okay, so I'm not usually into Kpop, but New Jeans has totally blown me away. Plus, I'm gonna talk about this lawsuit going on between New Jeans' producer and Hybe that will really impact New Jeans' future. (which is tragic for a fan like me)
 
 ### Who are they?
 
