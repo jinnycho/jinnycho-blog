df1c08be733a329364530be4b8506bda6b69ef5c
jinnycho <eunjincho503@gmail.com>
2024-05-01 00:17:29 -0400

    Adding more contents

--- src/js/output.js
+++ src/js/output.js
@@ -28059,13 +28059,43 @@ It was a delightful 4 weeks. Waking up early and walking there for 40 minutes in
 </audio>
 \u{1F430} my favorite hit track
 
-I've got to tell you all about this Kpop group I'm obsessed with right now. Okay, so I'm not usually into Kpop, but New Jeans has totally blown me away. Plus, I'm gonna talk about this lawsuit going on between New Jeans' producer and Hybe that will really impact New Jeans' future. (which is tragic for a fan like me)
+I've got to tell you all about this Kpop group I've been obsessed with. Okay, so I'm not usually that into Kpop, but New Jeans has totally blown me away. Plus, I'm gonna talk about this lawsuit going on between New Jeans' producer and Hybe that will really impact New Jeans' future. (which is tragic for a fan like me)
 
 ### Who are they?
+![newjeans2 alt ><](https://github.com/jinnycho/jinnycho.github.io/blob/main/src/assets/photos/newjeans2.gif?raw=true)
 
+Since their debut in July 2022, New Jeans has been sensational. They first grabbed people's attention for being the first girl group put together by Heejin Min, one of Korea's most successful producers. But then their music, especially tracks like "Attention" and "Hype Boy," and literally everything about them (dance, fashion, collabs with Powerpuff Girls etc) made every Korean fall in love with them pretty immediately. What's pretty cool is that their popularity also comes from the whole Y2K vibe they've got going on, which has helped them connect with fans ranging from teenagers to people in their 30s and 40s.
 
-### Their music
+![newjeans3 alt ><](https://i.namu.wiki/i/OEYkiYCB3g4qvejUL-tZeO9vA5xeNwDZ84mprkA5j-sU6BGurYjfqb0Y_DQJ5k67jPJPr5LiM1_pW_-0mFhdKQjuf7bgKegT4XkTIC-KuND8vZ-lWm1uOYNeHqPDqOWwfEeCtzeGBNbRpQSYj0AJTw.webp)
 
+And of course, they've been taking over the world! Less than a year after debuting, they were already killing it as the most streamed female Kpop act on Spotify in the US, top 10 artists on China's QQ Music charts, and they were the first international group to be nominated and awarded for the Japan Record Award. That's huge! Oh, and let's not forget they got invited to perform at Lollapalooza with a massive crowd of 70,000 people showing up just for them and Tokyo Rock Festival where 30,000 people showed up at "noon" in scorching summer weather. Now they're even planning a fan meeting at Tokyo Dome City, which is pretty much unheard of for a group that's only been around for like two years and hasn't even officially debuted in Japan yet.
+
+Each member is Minji (Korean), Danielle (Korean + Australian), Hyerin (Korean), Hani (Vietnamese + Australian), Hyein (Korean). Go Korea and Australia!
+
+### Music!
+:D I'll share some of my favorite tracks!
+
+#### (1) Attention
+<div style="display: flex; justify-content: center;">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/x8RIixqumUc?si=M71G1P6gHo5SxAoN" title="NewJeans Attention" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+#### (2) OMG
+<div style="display: flex; justify-content: center;">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/sVTy_wmn5SU?si=ypeFJ51qKtaUVy4d" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+#### (3) Get Up
+<div style="display: flex; justify-content: center;">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/SXM1q0CTfew?si=IJFJ7y943UCQ9HVH" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+#### (4) Hurt
+<div style="display: flex; justify-content: center;">
+  <iframe width="560" height="315" src="https://www.youtube.com/embed/tVIXY14aJms?si=MwWqo9dPtA8tDWel" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+</div>
+
+"Super Shy," "Ditto," "Hype Boy," "New Jeans", etc - every single one is a total jam. Also there's a new album release very soon!
 
 ### What's the lawsuit again?
 
